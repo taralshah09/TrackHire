9709cf034df2e9905598769ea8e99cd329c0fa46
Taral Shah <taralshah992005@gmail.com>
2026-02-23 12:39:19 +0530

    edit: Login error fixed

--- backend/src/main/java/com/projects/JobTracker_Backend/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/projects/JobTracker_Backend/repository/RefreshTokenRepository.java
@@ -1,11 +1,12 @@
 package com.projects.JobTracker_Backend.repository;
 
 import com.projects.JobTracker_Backend.model.RefreshToken;
+import com.projects.JobTracker_Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
 }
-
